d41eb15042d404bb48389e9a1576f4773fb15b7a
CoNike <32361787+CoNike@users.noreply.github.com>
2019-02-17 16:04:39 +0800

    Update 403_RNN_regression.py
    Modify：
    1> 47-51 lines ，a little faster when use the RMSPropOptimizer
    2> add LOSS figure
    3> change 58 lines (60 to 100)

--- tutorial-contents/403_RNN_regression.py
+++ tutorial-contents/403_RNN_regression.py
@@ -42,14 +42,20 @@ net_outs2D = tf.layers.dense(outs2D, INPUT_SIZE)
 outs = tf.reshape(net_outs2D, [-1, TIME_STEP, INPUT_SIZE])          # reshape back to 3D
 
 loss = tf.losses.mean_squared_error(labels=tf_y, predictions=outs)  # compute cost
-train_op = tf.train.AdamOptimizer(LR).minimize(loss)
+
+
+global_step = tf.Variable(0, trainable=False)
+starter_learning_rate = 0.06
+learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, 15, 0.35, staircase=True)
+train_op = tf.train.RMSPropOptimizer(learning_rate).minimize(loss, global_step=global_step)
+loss_his = []
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())     # initialize var in graph
 
 plt.figure(1, figsize=(12, 5)); plt.ion()       # continuously plot
 
-for step in range(60):
+for step in range(100):
     start, end = step * np.pi, (step+1)*np.pi   # time range
     # use sin predicts cos
     steps = np.linspace(start, end, TIME_STEP)
@@ -59,10 +65,12 @@ for step in range(60):
         feed_dict = {tf_x: x, tf_y: y}
     else:                                           # has hidden state, so pass it to rnn
         feed_dict = {tf_x: x, tf_y: y, init_s: final_s_}
-    _, pred_, final_s_ = sess.run([train_op, outs, final_s], feed_dict)     # train
-
+    _, pred_, final_s_, loss_ = sess.run([train_op, outs, final_s, loss], feed_dict)     # train
+    loss_his.apped(loss_)
     # plotting
     plt.plot(steps, y.flatten(), 'r-'); plt.plot(steps, pred_.flatten(), 'b-')
     plt.ylim((-1.2, 1.2)); plt.draw(); plt.pause(0.05)
 
 plt.ioff(); plt.show()
+plt.figure(2); plt.plot(np.arange(100), loss_his, 'r-')
+plt.show()
